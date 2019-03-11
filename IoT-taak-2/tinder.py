5890068fd2b2624203ee431abbfc514f97b72f56
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2019-03-11 23:43:44 +0100

    Final

--- IoT-taak-2/tinder.py
+++ IoT-taak-2/tinder.py
@@ -10,11 +10,10 @@ display_colour = (100, 30, 150)
 def fetchProfile():
     print('Fetch new profile')
 
-    url = 'https://randomuser.me/api/?results=3'
-    json_data = requests.get(url).json()['results'][0] 
+    url = 'https://randomuser.me/api/?'
+    json_data = requests.get(url).json()['results'][0]
     global profileObj
     global profileStr
-    global led_string 
 
     profileObj = {
         'firstname': str(json_data['name']['first']),
